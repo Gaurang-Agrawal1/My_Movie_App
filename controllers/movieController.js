fa78504dd26f917a0231fc91a0c3577b18ced02a
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2024-06-17 20:51:17 +0530

    Update movieController.js
    upadted

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -9,16 +9,23 @@ exports.searchMovies = async (req, res) => {
         return res.render('movies/search', { movies: [], lists: [] }); 
     }
 
-    try {
+  try {
         // Fetch all lists
-        const lists = await MovieList.find({}); // Modify this to fetch all lists
-
+        const lists = await MovieList.find({});
+        const userId = req.user.id;
+    
+        // Filter lists: include public lists or lists created by the current user
+        const filteredLists = lists.filter(list => list.isPublic || list.user.toString() === userId);
+    
+        // Make the OMDB API request
         const response = await axios.get(`http://www.omdbapi.com/?s=${title}&apikey=${process.env.OMDB_API_KEY}`);
         const data = response.data;
+    
         if (data.Response === 'False') {
-            return res.render('movies/search', { movies: [], lists: lists }); // Pass fetched lists
+            return res.render('movies/search', { movies: [], lists: filteredLists });
         }
-        res.render('movies/search', { movies: data.Search, lists: lists }); // Pass fetched lists
+    
+        res.render('movies/search', { movies: data.Search, lists: filteredLists });
     } catch (err) {
         console.log(err);
         res.status(500).send('Server error');
